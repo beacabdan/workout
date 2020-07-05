39e1feaeeb0ccf906c1ec119538924de56d023cc
Beatriz <beatriz.cabrero@upf.edu>
2020-07-05 12:29:55 +0200

    Weekday workout

--- code.js
+++ code.js
@@ -1,7 +1,19 @@
+var weekday = new Array(7);
+weekday[0] = "Sunday";
+weekday[1] = "Monday";
+weekday[2] = "Tuesday";
+weekday[3] = "Wednesday";
+weekday[4] = "Thursday";
+weekday[5] = "Friday";
+weekday[6] = "Saturday";
+
+
 function init()
 {
+    var d = new Date();
+    var n = d.getDay()
     var x = document.createElement("h1");
-    var t = document.createTextNode("Hello");
+    var t = document.createTextNode(weekday[d.getDay()] + " Workout");
     x.appendChild(t);
     document.body.appendChild(x);
 }
